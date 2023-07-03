f29709975cc589225f57668a2a6415ee90301887
RobertDickie182 <robertdanieldickie@gmail.com>
2023-07-03 21:50:10 +0100

    homework commit

---
+++ homework.js
@@ -0,0 +1,61 @@
+// const scenario = {
+//     murderer: 'Miss Scarlet',
+//     room: 'Library',
+//     weapon: 'Rope'
+// };
+
+// const declareMurderer = function () {
+//     return `The murderer is ${scenario.murderer}.`;
+// }
+
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// The output will be 'The murderer is Miss Scarlet',
+// the const verdict variable is calling the function above it which has already been determined to return a string with the murderer's name in it.
+
+// const murderer = 'Professor Plum';
+
+// const changeMurderer = function () {
+//     murderer = 'Mrs. Peacock';
+// }
+
+// const declareMurderer = function () {
+//     return `The murderer is ${murderer}.`;
+// }
+
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// I think there will be an error here due to a conflict between the two conflicts, I am not sure if that's right 
+
+// let murderer = 'Professor Plum';
+
+// const declareMurderer = function () {
+//     let murderer = 'Mrs. Peacock';
+//     return `The murderer is ${murderer}.`;
+// }
+
+// const firstVerdict = declareMurderer();
+// console.log('First Verdict: ', firstVerdict);
+
+// const secondVerdict = `The murderer is ${murderer}.`;
+// console.log('Second Verdict: ', secondVerdict);
+
+// I thought the output would be Mrs Peacock on both counts and i'm unsure why it isn't
+
+// let suspectOne = 'Miss Scarlet';
+// let suspectTwo = 'Professor Plum';
+// let suspectThree = 'Mrs. Peacock';
+
+// const declareAllSuspects = function () {
+//     let suspectThree = 'Colonel Mustard';
+//     return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+// }
+
+// const suspects = declareAllSuspects();
+// console.log(suspects);
+// console.log(`Suspect three is ${suspectThree}.`);
+
+// I expect the outut to be 'The suspects are Miss Scarlet, Professor Plum, and Colonel Mustard 
